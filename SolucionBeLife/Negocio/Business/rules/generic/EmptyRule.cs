cc6572d7cd8b72f369c723fdf512fb5ec3675093
Sebastian Castro <sxdogma@hotmail.com>
2018-10-16 18:33:24 -0300

    Clases exception

---
+++ SolucionBeLife/Negocio/Business/rules/generic/EmptyRule.cs
@@ -0,0 +1,26 @@
+﻿using Negocio.Business.exception;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.Business.rules.generic
+{
+    public class EmptyRule
+    {
+        public void isEmpty(string nameElement,
+            string value)
+        {
+
+            if (string.IsNullOrEmpty(value))
+            {
+
+                throw new BusinessException(
+                    "EL CAMPO " + nameElement +
+                    " NO PUEDE ESTAR EN BLANCO");
+            }
+        }
+    }
+}
+}

055b90460093cdd040e6c0866447e1214684b56b
javiera caramo <jccc12351@gmail.com>
2018-11-05 04:27:12 -0300

    listar clientes y contratos, .aspx y .cs

--- SolucionBeLife/SolucionBeLife/app/site/listar_cliente.aspx.cs
+++ SolucionBeLife/SolucionBeLife/app/site/listar_cliente.aspx.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Negocio.Business.rules;
+using Negocio.Business.rules.impl;
+using Persistencia.dao.entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +12,24 @@ namespace SolucionBeLife.app.site
 {
     public partial class listar_cliente : System.Web.UI.Page
     {
+        private static readonly ClienteBusiness clienteBusiness = new ClienteBusinessImpl();
+                
+        private List<ClienteEntity> clientes = clienteBusiness.listCliente();
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void refrescarTabla()
+        {
+            tbl_clientes.DataSource = clientes;
+            tbl_clientes.DataBind();
+        }
+
+        protected void btn_listar_clientes_Click(object sender, EventArgs e)
+        {
+            refrescarTabla();
         }
     }
 }

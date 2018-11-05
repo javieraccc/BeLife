9ef899dc24a4cf33b3f1894ef10e256e991f3a66
javiera caramo <jccc12351@gmail.com>
2018-11-05 02:58:07 -0300

    intefaz

--- SolucionBeLife/SolucionBeLife/app/site/template/template.Master.designer.cs
+++ SolucionBeLife/SolucionBeLife/app/site/template/template.Master.designer.cs
@@ -31,6 +31,51 @@ namespace SolucionBeLife.app.site.template {
         protected global::System.Web.UI.HtmlControls.HtmlForm frm_main_content;
         
         /// <summary>
+        /// Control imagen1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image imagen1;
+        
+        /// <summary>
+        /// Control image1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image image1;
+        
+        /// <summary>
+        /// Control image2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image image2;
+        
+        /// <summary>
+        /// Control image3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image image3;
+        
+        /// <summary>
+        /// Control image4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image image4;
+        
+        /// <summary>
         /// Control content.
         /// </summary>
         /// <remarks>
